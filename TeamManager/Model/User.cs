157cc6fecb83151e4d675aa23cf38ca0aa8265da
AlexsandroCastro <48591654+AlexsandroCastro@users.noreply.github.com>
2019-11-05 20:02:23 -0300

    tela inicial
    primeiro commit

---
+++ TeamManager/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamManager.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public String name { get; set; }
+
+        public String email { get; set; }
+
+        public String age { get; set; }
+    }
+} 
